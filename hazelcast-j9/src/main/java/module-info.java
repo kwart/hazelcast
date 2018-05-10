1ea77a6e691195e9363e04f146c0f777444206b1
Josef Cacek <josef.cacek@gmail.com>
2018-05-10 13:41:27 +0200

    MultiRelease JAR POC

---
+++ hazelcast-j9/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.hazelcast {
+    requires java.logging;
+    requires java.management;
+    requires java.scripting;
+    requires java.sql;
+    requires java.xml;
+    requires jdk.unsupported;
+
+}
